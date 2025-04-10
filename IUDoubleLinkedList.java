6f575948901d7676f778ed0ca1a68bce13c3da1b
Evan Wallace <evanwallace@u.boisestate.edu>
2025-04-10 16:04:17 -0600

    work

--- IUDoubleLinkedList.java
+++ IUDoubleLinkedList.java
@@ -148,7 +148,7 @@ public class IUDoubleLinkedList<T> implements IndexedUnsortedList<T> {
             }
         }
         nodeReturn = currNode;
-        
+
 
         return nodeReturn.getElement();
     }
@@ -186,7 +186,7 @@ public class IUDoubleLinkedList<T> implements IndexedUnsortedList<T> {
     @Override
     public Iterator<T> iterator() {
         // TODO Auto-generated method stub
-        return null;
+        return new DLLlistIterator();
     }
 
     @Override
@@ -198,16 +198,72 @@ public class IUDoubleLinkedList<T> implements IndexedUnsortedList<T> {
     @Override
     public ListIterator<T> listIterator() {
         // TODO Auto-generated method stub
-        return null;
+        return new DLLlistIterator();
     }
 
     @Override
     public ListIterator<T> listIterator(int startingIndex) {
         // TODO Auto-generated method stub
-        return null;
+        return new DLLlistIterator();
     }
 
-    
+    private class DLLlistIterator implements ListIterator<T> {
+
+        @Override
+        public void add(T e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public boolean hasNext() {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+        @Override
+        public T next() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public int nextIndex() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public T previous() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public int previousIndex() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public void remove() {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void set(T e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+    }
 
     @Override
     public T remove(int index) {
